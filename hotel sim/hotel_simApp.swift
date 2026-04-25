fe6df203938f75f6d359babcc0f858ac793179ff
leshamarshall <tomvit16@gmail.com>
2026-04-25 19:40:56 +0300

    Initial Commit

---
+++ hotel sim/hotel_simApp.swift
@@ -0,0 +1,17 @@
+//
+//  hotel_simApp.swift
+//  hotel sim
+//
+//  Created by Леша Маршал on 25.04.26.
+//
+
+import SwiftUI
+
+@main
+struct hotel_simApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
